304cafa8c6870b793d4054bc0851adc8738eca6f
nshadybekov <shadybekov@cointelegraph.com>
2022-07-23 20:49:07 +0300

    key value store in service implemented

--- modules/dino_roar/src/Controller/RoarController.php
+++ modules/dino_roar/src/Controller/RoarController.php
@@ -10,6 +10,6 @@ class RoarController extends ControllerBase
     public function roar($count)
     {
         $generator = \Drupal::service('dino_roar.roar_generator');
-        return new Response($generator->getRoar());
+        return new Response($generator->getRoar(13));
     }
 }
--- modules/dino_roar/src/Jurassic/RoarGenerator.php
+++ modules/dino_roar/src/Jurassic/RoarGenerator.php
@@ -2,10 +2,31 @@
 
 namespace Drupal\dino_roar\Jurassic;
 
+use Drupal\Core\KeyValueStore\KeyValueFactory;
+
 class RoarGenerator
 {
-    public function getRoar()
+    private KeyValueFactory $keyValueFactory;
+
+    public function __construct(KeyValueFactory $keyValueFactory)
     {
-        return 'Burp!';
+        $this->keyValueFactory = $keyValueFactory;
+    }
+
+    public function getRoar($length)
+    {
+        $key = 'roar' . $length;
+        $store = $this->keyValueFactory->get('dino');
+
+        if($store->has($key)) {
+            return $store->get($key);
+        }
+
+        sleep(2);
+
+        $str = 'B'.str_repeat('u',$length).'rp!';
+        $store->set($key, $str);
+
+        return $str;
     }
 }
